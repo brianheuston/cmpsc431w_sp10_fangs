d352d4daaa042937709694a30a79e22a15d5b445
Brian Heuston <bah5069@gmail.com>
2010-04-11 18:38:12 -0400

    Initial Commit by Brian Heuston

---
+++ src/java/cmpsc431w_sp10_fangs/ExampleClass.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cmpsc431w_sp10_fangs;
+
+/**
+ *
+ * @author Brian Heuston
+ */
+public class ExampleClass {
+
+}
